a4bc61adce3a01a0c27f6335d72051bdfbc9a9e0
Ahmed Emam <ahmed.emam.dev@gmail.com>
2020-08-28 04:09:45 -0700

    =settings and authentication redirect

--- app/Http/Controllers/Dashboard/LoginController.php
+++ app/Http/Controllers/Dashboard/LoginController.php
@@ -7,25 +7,40 @@ use App\Http\Requests\AdminLoginRequest;
 
 class LoginController extends Controller
 {
-     public function login(){
+    public function login()
+    {
 
-         return view('dashboard.auth.login');
-     }
+        return view('dashboard.auth.login');
+    }
 
 
-     public function postLogin(AdminLoginRequest  $request)
-     {
+    public function postLogin(AdminLoginRequest $request)
+    {
 
-          //validation
+        //validation
 
-         //check , store , update
+        //check , store , update
 
-         $remember_me = $request->has('remember_me') ? true : false;
+        $remember_me = $request->has('remember_me') ? true : false;
 
-         if (auth()->guard('admin')->attempt(['email' => $request->input("email"), 'password' => $request->input("password")], $remember_me)) {
-              return redirect() -> route('admin.dashboard');
-         }
-          return redirect()->back()->with(['error' => 'هناك خطا بالبيانات']);
+        if (auth()->guard('admin')->attempt(['email' => $request->input("email"), 'password' => $request->input("password")], $remember_me)) {
+            return redirect()->route('admin.dashboard');
+        }
+        return redirect()->back()->with(['error' => 'هناك خطا بالبيانات']);
 
-     }
+    }
+
+    public function logout()
+    {
+
+        $gaurd = $this->getGaurd();
+        $gaurd->logout();
+
+        return redirect()->route('admin.login');
+    }
+
+    private function getGaurd()
+    {
+        return auth('admin');
+    }
 }
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (!$request->expectsJson()) {
-            if (Request::is('admin/*'))
+        if (!$request->expectsJson()){
+            if (Request::is(app()->getLocale().'/admin*'))
                 return route('admin.login');
             else
                 return route('login');
